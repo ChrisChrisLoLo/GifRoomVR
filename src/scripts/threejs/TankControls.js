e7523311adcaa83749ad8d2dd04c0ef514aa0295
ChrisChrisLoLo <sporewoh@gmail.com>
2019-07-10 07:00:56 -0700

    Start work on tank controls

--- src/scripts/threejs/TankControls.js
+++ src/scripts/threejs/TankControls.js
@@ -4,37 +4,77 @@ export default class TankControls{
     constructor(camera){
         this.camera = camera;
         this.enabled = false;
+        //Current direction being given
+        this.direction = {forward:false,backward:false,left:false,right:false};
         // this.raycaster = new Raycaster();
         // this.mouse = new Vector2();
 
 
         this.connect = this.connect.bind(this);
         this.disconnect = this.disconnect.bind(this);
-        this.onMouseDown = this.onMouseDown.bind(this);
-        this.onMouseUp = this.onMouseUp.bind(this);
+        this.onKeyDown = this.onKeyDown.bind(this);
+        this.onKeyUp = this.onKeyUp.bind(this);
     }
 
     connect(){
-        window.addEventListener( 'mousedown', this.onMouseDown, false );
-        window.addEventListener( 'mousedown', this.onMouseUp, false );
+        window.addEventListener( 'keydown', this.onKeyDown, false );
+        window.addEventListener( 'keyup', this.onKeyUp, false );
         this.enabled = true;
     };
 
     disconnect(){
-        window.removeEventListener( 'mousedown', this.onMouseDown, false );
-        window.removeEventListener( 'mousedown', this.onMouseUp, false );
+        window.removeEventListener( 'keydown', this.onKeyDown, false );
+        window.removeEventListener( 'keyup', this.onKeyUp, false );
         this.enabled = false;
     };
 
-    onMouseDown(e){
-        // calculate mouse position in normalized device coordinates
-        // (-1 to +1) for both components
-    };
+    update(){
+        if(this.enabled){
+            
+        }
+    }
 
+    onKeyDown(e){
+        let dir = this.direction;
+        switch(e.keyCode){
+            case 38:
+                dir.forward = true;
+                break;
+            case 40:
+                dir.backward = true;
+                break;
+            case 37:
+                dir.left = true;
+                break;
+            case 39:
+                dir.right = true;
+                break;
+            default:
+                break;
+                //spin toggle
+            // case 32:
+            //     dir.forward = true;
+            //     break;
+        }
+    }
 
-
-    onMouseUp(e){
-        this.mouse.x = ( e.clientX / window.innerWidth ) * 2 - 1;
-        this.mouse.y = - ( e.clientY / window.innerHeight ) * 2 + 1;
-    };
+    onKeyUp(e){
+        let dir = this.direction;
+        switch(e.keyCode) {
+            case 38:
+                dir.forward = false;
+                break;
+            case 40:
+                dir.backward = false;
+                break;
+            case 37:
+                dir.left = false;
+                break;
+            case 39:
+                dir.right = false;
+                break;
+            default:
+                break;
+        }
+    }
 }
